9231e398739d353018a37a8d72ad5475415a7bbd
SamiraHaji <samirahaji69@gmail.com>
2022-08-01 19:35:29 +0200

    modifié

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,7 +7,7 @@ function Sidebar() {
          <div className='float-center'>
     <button type="button" className="p-10 block text-gray-400 hover:text-black cursor-pointer focus:text-gray-700 focus:outline-none">
     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-  <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
+  <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd" />
 </svg>
     </button>
     </div>
