1e3690340b85a712b2742236749d4a4034c9f3d9
SamiraHaji <samirahaji69@gmail.com>
2022-08-02 08:57:55 +0200

    new

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,15 @@ export default function Home() {
   const search =(e) => {
   e.preventDefault(); 
   const term = agenceRef.current.value;
-
+  const term1 =serviceseRef.current.value;
+  const term2 =contactRef.current.value;
   if(!term) return;
 
 router.push(`/agence?term=${term}`);
+
+router.push(`/services?term=${term1}`);
+
+router.push(`/contact?term=${term2}`);
   }
   return (
 <main className="flex flex-col items-center justify-center" >
